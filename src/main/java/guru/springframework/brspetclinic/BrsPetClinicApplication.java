0300368ecbff0526570d1a5a68b8481885141e43
bigredsparks <svsparks@swbell.net>
2019-02-20 14:28:53 -0600

    Initial Project Add

---
+++ src/main/java/guru/springframework/brspetclinic/BrsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.brspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrsPetClinicApplication.class, args);
+    }
+
+}
