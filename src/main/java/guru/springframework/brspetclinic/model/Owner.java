f2248090370ceb6a3df4be7edc8d573f19d7b54c
bigredsparks <svsparks@swbell.net>
2019-02-21 09:27:26 -0600

    Adding POJOs.  Closes #2

---
+++ src/main/java/guru/springframework/brspetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.brspetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/brspetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.brspetclinic.model;
+
+public class Vet extends Person {
+}
